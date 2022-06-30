2d5c2a8babe80a8f69d899d50890fd4f91ae6062
lucas <lpatronr@gmail.com>
2022-06-30 12:17:20 -0500

    Enhance API validation

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -16,7 +16,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     switch (req.method) {
       case 'GET':
         try {
-          if (typeof req.query.id !== 'string' || !req.query.id.match(/^\d+$/))
+          if (!req.query.id || typeof req.query.id !== 'string' || !req.query.id.match(/^\d+$/))
             return res.status(400).json({ message: 'Query is invalid' });
 
           const user: User | null = await getUser(req.query.id);
@@ -32,13 +32,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           if (typeof cc !== 'string' || !cc.match(/^\d+$/))
             return res.status(400).json({ message: 'Invalid cc' });
 
-          if (!Array.isArray(dogs) || !dogs.every((dog: any) => typeof dog === 'string'))
+          if (
+            !Array.isArray(dogs) ||
+            !dogs.every((dog: any) => typeof dog === 'string') ||
+            dogs.length > 3 ||
+            dogs.length < 1
+          )
             return res.status(400).json({ message: 'Invalid dogs' });
 
           const findUser = await getUser(cc);
           if (findUser) return res.status(400).json({ message: `${cc} already exists` });
 
-          const newUser = await createUser(cc, dogs);
+          await createUser(cc, dogs);
           return res.status(200).json({ message: 'User created' });
         } catch (e) {
           return res.status(400).json({ message: e });
@@ -49,7 +54,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           if (typeof cc !== 'string' || !cc.match(/^\d+$/))
             return res.status(400).json({ message: 'Invalid cc' });
 
-          if (!Array.isArray(dogs) || !dogs.every((dog: any) => typeof dog === 'string'))
+          if (
+            !Array.isArray(dogs) ||
+            !dogs.every((dog: any) => typeof dog === 'string') ||
+            dogs.length > 3 ||
+            dogs.length < 1
+          )
             return res.status(400).json({ message: 'Invalid dogs' });
 
           const user = await updateUser(cc, dogs);
